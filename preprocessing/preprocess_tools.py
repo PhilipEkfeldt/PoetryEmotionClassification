1349e70502cc6fc010e9d13b7282fb8b3ddb938a
Philip Ekfeldt <philipekfeldt@gmail.com>
2019-05-11 23:29:44 -0400

    Lyrics cleaning

--- preprocessing/preprocess_tools.py
+++ preprocessing/preprocess_tools.py
@@ -42,3 +42,41 @@ def clean_poem(text):
 
 
     return cleaned_text
+
+def clean_lyrics(text):
+
+    cleaned_text = text
+
+    #Remove html character
+    cleaned_text = cleaned_text.replace("&nbsp", " ")
+    #Remove links
+    cleaned_text = re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b', '', cleaned_text, flags=re.MULTILINE)
+
+
+    #Remove stuff within square brackets
+    cleaned_text = re.sub("\[.*?\]", "", cleaned_text)
+    #Remove another html/markdown character
+    cleaned_text = cleaned_text.replace("x200B", "")
+    #Replace linebreaks and tabs
+    cleaned_text = cleaned_text.replace("\n", " ")
+    cleaned_text = cleaned_text.replace("\r", " ")
+    cleaned_text = cleaned_text.replace("\t", " ")
+    cleaned_text = cleaned_text.replace("\x14", " ")
+
+     # Replace quotation marks
+    cleaned_text = cleaned_text.replace('”', "")
+    cleaned_text = cleaned_text.replace("“", "")
+
+    #Replace dash
+    cleaned_text = cleaned_text.replace("—", " ")
+
+    # Remove punctuations
+    cleaned_text = "".join(c for c in cleaned_text if not c in punctuation)
+
+    #Remove extra whitespace
+    cleaned_text = ' '.join(cleaned_text.split())
+
+
+
+
+    return cleaned_text
