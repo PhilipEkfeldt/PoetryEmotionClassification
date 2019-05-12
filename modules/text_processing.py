2b38a21ae625e2a7a1172c4fa8db952164efcac3
Philip Ekfeldt <philipekfeldt@gmail.com>
2019-05-12 19:25:21 -0400

    Fixed lyrics pipeline

--- modules/text_processing.py
+++ modules/text_processing.py
@@ -6,6 +6,10 @@ import torch
 from torchtext.data import Field, TabularDataset, Iterator, BucketIterator
 from torchtext import vocab
 from sklearn.model_selection import train_test_split
+import sys
+import csv
+
+csv.field_size_limit(sys.maxsize)
 
 spacy_en = spacy.load('en')
 #Structured code made originally by Micaela
@@ -26,12 +30,48 @@ def read_split_file(data_source, tar_path):
 
     return train_path, val_path, test_path
 
+def read_split_file_lyrics(data_source, tar_path):
+    df = pd.read_csv(data_source)
+    #split into train/val/test
+    #df = df[:1000]
+    df["pos"] =  (df["label"] == 1)*1
+    df["neg"] =  (df["label"] == -1)*1
+    df = df.drop("label", axis=1)
+    df_train, df_val = train_test_split(df, test_size = 0.2)
+    
+    #write out to train/val/test csv
+    train_path, val_path = [ tar_path  + s for s in ["lyrics_train.csv", "lyrics_val.csv"] ]
+
+    df_train.to_csv(train_path,index=False)
+    df_val.to_csv(val_path,index=False) 
+
+    return train_path, val_path
+
 def tokenizer(text): 
     return [tok.text for tok in spacy_en.tokenizer(text)]
 
 
 
 class BatchWrapper:
+    '''
+    NOTE: 
+    BucketIterator returns a Batch object instead of text index and labels. 
+    Also, the Batch object is not iterable like pytorch Dataloader
+    This is a wrapper to extract the text and labels + make the Batch iterable
+    '''
+    def __init__(self, dl, x_field, y_field):
+        self.dl, self.x_field, self.y_field = dl, x_field, y_field
+    def __len__(self):
+        return len(self.dl)
+    
+    def __iter__(self):
+        for batch in self.dl:
+            X = getattr(batch, self.x_field)
+            # concatenate y into a single tensor
+            y = torch.cat([getattr(batch, feat).unsqueeze(1) for feat in self.y_field], dim=1).float()
+            yield (X,y)
+
+class BatchWrapperLyrics:
     '''
     NOTE: 
     BucketIterator returns a Batch object instead of text index and labels. 
@@ -59,9 +99,9 @@ def generate_iterators(train_path, val_path, test_path, batch_size, device="cpu"
                     ("joy", LABEL),("love", LABEL),
                     ("optimism", LABEL),('pess', LABEL),('sad', LABEL)]
     train, val, test = TabularDataset.splits(
-                path='Data/',
-                train='poems_train.csv', validation="poems_val.csv",
-                test='poems_test.csv',format='csv',
+                path='',
+                train=train_path, validation=val_path,
+                test=test_path,format='csv',
                 skip_header=True,fields=datafields)
     TEXT.build_vocab(train, vectors='glove.6B.300d')
 
@@ -79,3 +119,24 @@ def generate_iterators(train_path, val_path, test_path, batch_size, device="cpu"
                         sort=False, sort_within_batch=False, repeat=True)
 
     return train_iter, val_iter, test_iter, TEXT
+
+def generate_iterators_lyrics(train_path, val_path, batch_size, device="cpu"):
+    TEXT = Field(sequential=True, tokenize=tokenizer, lower=True)
+    LABEL = Field(sequential=False, use_vocab=False)
+
+    datafields = [("lyrics", TEXT), ("pos", LABEL), ("neg", LABEL)]
+    train, val = TabularDataset.splits(
+                path='',
+                train=train_path, validation=val_path,format='csv',
+                skip_header=True,fields=datafields)
+    TEXT.build_vocab(train, vectors='glove.6B.300d')
+
+    train_iter = BucketIterator(train, batch_size=batch_size, device=device, 
+                                sort_key = lambda x: len(x.text), 
+                                sort_within_batch=False, repeat=False)
+    val_iter = BucketIterator(val, batch_size=batch_size, device=device, 
+                              sort_key = lambda x: len(x.text), 
+                                sort_within_batch=False, repeat=False)
+
+
+    return train_iter, val_iter, TEXT
